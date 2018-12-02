1a4ec6a8ebff5149165cd52f1c5a5998de434c7c
Lindsay Karty <lmkarty@gmail.com>
2018-12-02 12:35:28 -0500

    Added fields to construtor but couldn't get image or artist to return

--- src/main/java/com/finalproject/ArtHeistApp/entities/Artworks.java
+++ src/main/java/com/finalproject/ArtHeistApp/entities/Artworks.java
@@ -1,5 +1,6 @@
 package com.finalproject.ArtHeistApp.entities;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties (ignoreUnknown=true)
@@ -7,33 +8,96 @@ public class Artworks {
 	
 	private Integer objectID;
 	private String title;
+	private Integer objectEndDate;
+	private String dimensions;
+	private String primaryImage;
+	private String artistDisplayName;
 	
 	
 	public Artworks() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Artworks(Integer objectID, String title) {
+
+
+	public Artworks(Integer objectID, String title, Integer objectEndDate, String dimensions, String primaryImage,
+			String artistDisplayName) {
 		super();
 		this.objectID = objectID;
 		this.title = title;
+		this.objectEndDate = objectEndDate;
+		this.dimensions = dimensions;
+		this.primaryImage = primaryImage;
+		this.artistDisplayName = artistDisplayName;
 	}
+
+
 	public Integer getObjectID() {
 		return objectID;
 	}
+
+
 	public void setObjectID(Integer objectID) {
 		this.objectID = objectID;
 	}
+
+
 	public String getTitle() {
 		return title;
 	}
+
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+
+	public Integer getObjectEndDate() {
+		return objectEndDate;
+	}
+
+
+	public void setObjectEndDate(Integer objectEndDate) {
+		this.objectEndDate = objectEndDate;
+	}
+
+
+	public String getDimensions() {
+		return dimensions;
+	}
+
+
+	public void setDimensions(String dimensions) {
+		this.dimensions = dimensions;
+	}
+
+
+	public String getPrimaryImage() {
+		return primaryImage;
+	}
+
+
+	public void setPrimaryImage(String primaryImage) {
+		this.primaryImage = primaryImage;
+	}
+
+
+	public String getName() {
+		return artistDisplayName;
+	}
+
+
+	public void setName(String artistDisplayName) {
+		this.artistDisplayName = artistDisplayName;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Artworks [objectID=" + objectID + ", title=" + title + "]";
+		return "Artworks [objectID=" + objectID + ", title=" + title + ", objectEndDate=" + objectEndDate
+				+ ", dimensions=" + dimensions + ", primaryImage=" + primaryImage + ", artistDisplayName=" + artistDisplayName + "]";
 	}
+
 	
 	
 	
