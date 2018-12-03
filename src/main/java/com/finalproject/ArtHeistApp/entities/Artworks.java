0df060ad99730eb6f842fdad4dc765e7bc7f48c9
AjaBurnett <burnettaja@gmail.com>
2018-12-03 09:18:45 -0500

    resolved artist/image issue

--- src/main/java/com/finalproject/ArtHeistApp/entities/Artworks.java
+++ src/main/java/com/finalproject/ArtHeistApp/entities/Artworks.java
@@ -1,4 +1,4 @@
-package com.finalproject.ArtHeistApp.entities;
+ package com.finalproject.ArtHeistApp.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -7,16 +7,21 @@ public class Artworks {
 	
 	private Integer objectID;
 	private String title;
-	
+	private String artistDisplayName;
+	private String dimensions;
+	private String primaryImage;
 	
 	public Artworks() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Artworks(Integer objectID, String title) {
+	public Artworks(Integer objectID, String title, String artistDisplayName, String dimensions, String primaryImage) {
 		super();
 		this.objectID = objectID;
 		this.title = title;
+		this.artistDisplayName =artistDisplayName;
+		this.dimensions=dimensions;
+		this.primaryImage= primaryImage;
 	}
 	public Integer getObjectID() {
 		return objectID;
@@ -30,12 +35,33 @@ public class Artworks {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	@Override
-	public String toString() {
-		return "Artworks [objectID=" + objectID + ", title=" + title + "]";
+	
+	public String getArtistDisplayName() {
+		return artistDisplayName;
+	}
+	public void setArtistDisplayName(String artistDisplayName) {
+		this.artistDisplayName = artistDisplayName;
+	}
+		
+	public String getDimensions() {
+		return dimensions;
+	}
+	public void setDimensions(String dimensions) {
+		this.dimensions = dimensions;
 	}
 	
 	
+	public String getPrimaryImage() {
+		return primaryImage;
+	}
+	public void setPrimaryImage(String primaryImage) {
+		this.primaryImage = primaryImage;
+	}
+	
+	@Override
+	public String toString() {
+		return "Artworks [objectID=" + objectID + ", title=" + title + ", artistDisplayName=" + artistDisplayName + ", dimensions=" + dimensions +", primaryImage=" + primaryImage +", ]";
+	}
 	
 }
 
